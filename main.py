1efe0bfed06bebcdd3e0b354a0e2393679303de4
borzxo <albert_elimh@yandex.ru>
2023-09-15 22:16:49 +0300

    API Router; CRUD; Annotated validation; install pydantic[email]

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from typing import Annotated
+from fastapi import FastAPI, Path
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
